bd871a7d37e012d1b474f992a0ec3fda9eb89638
ravish0007 <ravish.nfs@gmail.com>
2022-08-23 03:17:00 +0530

    Add 4 consecs basic generator

--- problem-47/main.py
+++ problem-47/main.py
@@ -3,7 +3,13 @@ import itertools
 def some_functionality(number1, number2, number3, number4):
     pass
 
-for number in itertools.count():
-    if some_functionality(number):
-        print(number)
-        break
+def four_consecutive_number_generator(start=0):
+    local_number = start
+    while True:
+        yield [local_number, local_number + 1,
+               local_number + 2, local_number + 3]
+        local_number += 1
+
+for first_number, second_number, third_number, fourth_number in four_consecutive_number_generator():
+    print(first_number, second_number, third_number, fourth_number)
+   
