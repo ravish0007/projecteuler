34a316451aa293e38c52683687ccbbe1a797bd07
ravish0007 <ravish.nfs@gmail.com>
2022-08-23 03:39:08 +0530

    defined functionality, a little

--- problem-47/main.py
+++ problem-47/main.py
@@ -1,15 +1,21 @@
-import itertools
-
-def some_functionality(number1, number2, number3, number4):
-    pass
+    
 
 def four_consecutive_number_generator(start=0):
     local_number = start
     while True:
         yield [local_number, local_number + 1,
                local_number + 2, local_number + 3]
-        local_number += 1
 
-for first_number, second_number, third_number, fourth_number in four_consecutive_number_generator():
-    print(first_number, second_number, third_number, fourth_number)
-   
+
+def get_factors(number):
+    return []
+
+
+def has_distinct_prime_factors(numbers):
+    for each_number in numbers:
+        factors = get_factors(each_number)
+
+for numbers in four_consecutive_number_generator():
+    if has_distinct_prime_factors(numbers):
+        print(numbers)
+        break
