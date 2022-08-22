a0b6cd27865d721fb5ec1b9a0724d85b4a7c8961
ravish0007 <ravish.nfs@gmail.com>
2022-08-23 04:36:07 +0530

    OM Nahmah Shivaya

--- problem-47/main.py
+++ problem-47/main.py
@@ -1,21 +1,40 @@
-    
+import math  
 
 def four_consecutive_number_generator(start=0):
     local_number = start
     while True:
         yield [local_number, local_number + 1,
                local_number + 2, local_number + 3]
+        local_number += 1
 
+# Hello problem-3, doesn't work for 0, 1 
+def get_prime_factors(number):
+    prime_factors = []
 
-def get_factors(number):
-    return []
+    while number % 2 == 0:
+        number = number / 2
+        prime_factors.append(2)
+         
+    for index in range(3, int(math.sqrt(number)) + 1, 2):
+        while number % index == 0:
+            prime_factors.append(index)
+            number = number / index
 
+    if number > 2:
+        prime_factors.append(number)
 
-def has_distinct_prime_factors(numbers):
+    return prime_factors
+
+def has_distinct_prime_factors(numbers, length):
     for each_number in numbers:
-        factors = get_factors(each_number)
+        factors = get_prime_factors(each_number)
+        if len(set(factors)) != length:  # distinct, final piece
+            return False
+    return True
 
-for numbers in four_consecutive_number_generator():
-    if has_distinct_prime_factors(numbers):
+
+for numbers in four_consecutive_number_generator(start=1):
+    print('processing: ', *numbers, flush=True)
+    if has_distinct_prime_factors(numbers, length=4):
         print(numbers)
         break
